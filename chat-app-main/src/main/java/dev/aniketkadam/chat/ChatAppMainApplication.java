7e0486d15b2355bb0c9bf89dc0dbbbfbcbdca30e
Aniket205kadam <aniketrkadam205@gmail.com>
2024-12-06 19:45:23 +0530

    websocket

---
+++ chat-app-main/src/main/java/dev/aniketkadam/chat/ChatAppMainApplication.java
@@ -0,0 +1,13 @@
+package dev.aniketkadam.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppMainApplication.class, args);
+	}
+
+}
---
+++ chat-app-main/src/test/java/dev/aniketkadam/chat/ChatAppMainApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aniketkadam.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
